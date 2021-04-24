c2e9d6929b2bfca040377cc9634e61d49b276522
AyeshaSahar <83140034+AyeshaSahar@users.noreply.github.com>
2021-04-25 01:53:05 +0500

    Add files via upload

---
+++ 1. Madlibs.py
@@ -0,0 +1,20 @@
+
+adj1= input("Adjective:")
+verb1= input("Verb:")
+noun1= input("Noun:")
+verb2= input("Verb:")
+verb3= input("Verb:")
+verb4= input("Verb:")
+adj2= input("Adjective:")
+famous_person= input("famous person:")
+verb5= input("Verb:")
+noun2= input("Noun:")
+
+madlib= f"Non-magic people (more commonly known as {adj1}) were particularly afraid of \
+magic in medieval times, but not very good at {verb1} it. On the rare {noun1} that they \
+did {verb2} a real witch or wizard, burning had no effect whatsoever. The witch or wizard would \
+perform a basic Flame {verb3} Charm and then pretend to {verb4} with pain while \
+enjoying a gentle, {adj2} sensation. Indeed, {famous_person} enjoyed being burned \
+so much that she {verb5} herself to be caught no less than forty seven times in various {noun2}."
+
+print(madlib)
