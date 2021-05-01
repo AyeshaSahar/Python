e94f337697df167146d15ae9120341f25f013750
AyeshaSahar <83140034+AyeshaSahar@users.noreply.github.com>
2021-05-01 23:13:28 +0500

    Add files via upload

---
+++ 3. Guess the number (computer has to guess).py
@@ -0,0 +1,23 @@
+import random
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low  # could also be high b/c low = high
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f'Yay! The computer guessed your number, {guess}, correctly!')
+
+
+computer_guess(10)
+
+
